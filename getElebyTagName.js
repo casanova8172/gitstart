6ff7771106746ca1eea81d2ded6dc9923c4ffdfc
casanova8172 <leelanandkatihar01@gmail.com>
2025-09-01 20:48:51 +0530

    get Ele by tag Name

---
+++ getElebyTagName.js
@@ -0,0 +1,7 @@
+// Write your code below:
+const litag = document.getElementsByTagName('li');
+litag[4].style.color = 'blue';
+
+for (let i = 0; i < litag.length; i++){
+    litag[i].style.fontStyle = 'italic';
+}
